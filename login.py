7a31f2be816eacd6a4460e6fe90810f277a9028d
Riyan Dwiki Rusdianto <33405400+RiyanDwikiRusdianto@users.noreply.github.com>
2017-12-17 18:38:29 +0700

    Add files via upload
    Program Login

---
+++ login.py
@@ -0,0 +1,6 @@
+username=(str(input("Masukan Username Anda : ")))
+Password=(str(input("Masukan Password Anda : ")))
+if(username=="admin" and Password=="1234"):
+	print("Anda Telah Login...")
+else:
+	print("Nama atau Password Anda benar")
